898b86be2486ced7dad9b9d2d14efd06758c521e
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 10:22:33 +0200

    ts one

---
+++ week-01/day-04/sample.ts
@@ -0,0 +1,25 @@
+const pi: number = 5.85;
+let times: number = 42;
+let myName: string = "Pascal";
+
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+console.log(10);
+
+console.log(times);
+console.log(myName);
+
+let itCanBeUndefined: number;
+
+console.log(itCanBeUndefined);
+console.log myName + 1 ;
