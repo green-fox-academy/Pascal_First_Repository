0c334fe92b9e055601c149c5ad6827edccf8369d
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 10:30:41 +0200

    hate -m

--- week-01/day-04/sample.ts
+++ week-01/day-04/sample.ts
@@ -2,19 +2,22 @@ const pi: number = 5.85;
 let times: number = 42;
 let myName: string = "Pascal";
 
+console.log(times);
+
+times= 15;
+
+console.log(times); 
+
+times = times + 5;
+
+console.log(times); 
+
+
 console.log(10);
 console.log(10);
 console.log(10);
 console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
-console.log(10);
+
 
 console.log(times);
 console.log(myName);
@@ -22,4 +25,15 @@ console.log(myName);
 let itCanBeUndefined: number;
 
 console.log(itCanBeUndefined);
-console.log myName + 1 ;
+
+
+let sentence1: boolean = true;
+let sentence2: boolean = false;
+
+console.log(sentence1);
+console.log(!sentence1);
+
+console.log(sentence1 && !sentence2);
+console.log(sentence1 || !sentence2);
+
+
