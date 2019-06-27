15fd487cd60184963e5714a5a50103bfc1cb820b
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 16:11:59 +0200

    pillanat

---
+++ week-01/day-04/workshop/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,14 @@
+/*
+
+let a: number = 0;
+while (a < 100) {
+  console.log('I might cheat on exams! HAHAHAHA'); // Prints the numbers from 0 to 9
+  a++;
+}
+
+*/
+
+
+for (let i: number = 0; i < 100; i++) {
+    console.log('I might cheat on exams! HAHAHAHA'); // Prints the numbers from 0 to 99
+  }
---
+++ week-01/day-04/workshop/print-even.ts
@@ -0,0 +1,9 @@
+// Create a program that prints all the even numbers between 0 and 500
+
+let a: number = 1;
+while (a <= 500) {{
+    if (a % 2 == 0)
+  console.log(a);
+} 
+  a++;
+}
