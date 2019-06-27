b4a65c6470c389968dd29188aff896202723ca85
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 17:34:59 +0200

    end

---
+++ week-01/day-04/workshop/draw-triangle.ts
@@ -0,0 +1,32 @@
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+
+//function generateTriangle(num) {
+//    let number = '';
+//  
+//    for (let i = 1; i <= num; i++) {
+//      console.log(number += '*');
+//    }
+//  }
+//  generateTriangle(10);
+
+  let lineCount: number = 10;
+
+  let a: number = 1;
+  let stars: string = '';
+
+  while (a <= lineCount) {
+
+  stars +- '*';
+
+  console.log(stars);
+  a++;
+  
+  }
