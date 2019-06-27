3f9fe0de608bc1e239a7c8d6e79d71a7dac4aacd
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 11:35:18 +0200

    yep

--- week-01/day-04/if.ts
+++ week-01/day-04/if.ts
@@ -16,9 +16,9 @@ if(temperature < 10) {
 
  if (num < 25) {
     console.log("small number");
- } else if (25 < = num && num < 50); {
+ } else if (25 <= num && num < 50) {
     console.log("medium number");
- } else if (50 < = num && num < 75); {
+ } else if (50 <= num && num < 75) {
     console.log("big number");
  } else {
     console.log("huge number");
