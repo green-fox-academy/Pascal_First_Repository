ea4f170a06b15375305bd5dc4d1487c7b09d1ad6
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 10:51:06 +0200

    hate -m

---
+++ week-01/day-04/if.ts
@@ -0,0 +1,25 @@
+let temperature: number = 35;
+
+if(temperature > 30) {
+    console.log("Melting my ass off!");
+} else {}
+
+if(temperature < 10) {
+    console.log("Popsicle yo!");
+}
+ if ((true && true) || (false && true)) { 
+     console.log("Whaaaaat?");
+ }
+
+
+ let num: number = 69;
+
+ if (num < 25) {
+     console.log("small number");
+ } else if (25 < = num && num < 50) {
+    console.log("medium number");
+ } else if (50 < = num && num < 75) {
+    console.log("big number");
+ } else {
+    console.log("huge number";)
+ }
