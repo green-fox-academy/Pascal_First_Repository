5afdeb6d5054a9e0da5c0aa03f6c3c585f90c867
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 14:27:42 +0200

    I am Batmaaaaaaaaaan

---
+++ week-01/day-04/workshop/define.ts
@@ -0,0 +1,15 @@
+let myName: string = "Pascal a tactical cheesestick";
+let myAge: number = 26;
+let myHeight: number = 1.83
+let married: boolean = false;
+
+
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+if (married) {
+    console.log("Fucked");
+} else {
+    console.log("Happy");
+}
