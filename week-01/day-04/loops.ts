90cd42986ddfca880dfafe00fc7765ec28bd13ec
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 11:14:06 +0200

    hate -m

--- week-01/day-04/loops.ts
+++ week-01/day-04/loops.ts
@@ -1,7 +1,13 @@
+/*
 let times: number = 0;
 
 
 while(times < 10) {
     console.log("notä" , times);
     times++ ;
+}
+*/
+
+for(let iterator: number = 0; iterator < 100; iterator++) {
+    console.log(iterator);
 }
