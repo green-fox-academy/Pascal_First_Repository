8c8f6815f6f61d8007fc436448ef9f5453ee11a6
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 12:43:55 +0200

    yeeeeeah boy

---
+++ week-01/day-04/workshop/coding-hours.ts
@@ -0,0 +1,7 @@
+let daily: number = 6;
+let semester: number = 17;
+let days: number = 5;
+let average: number = 17 * 52
+
+console.log(daily * days * semester);
+console.log((`${Math.round(100 / average * daily * days * semester)}%`));
