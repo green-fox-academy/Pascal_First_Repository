24b5916db2d48dd4c9cade037c5464c48360bcbf
Pascal Dawson <dawsonpascal@hotmail.com>
2019-06-27 10:59:38 +0200

    hate -m

--- week-01/day-04/if.ts
+++ week-01/day-04/if.ts
@@ -15,11 +15,11 @@ if(temperature < 10) {
  let num: number = 69;
 
  if (num < 25) {
-     console.log("small number");
- } else if (25 < = num && num < 50) {
+    console.log("small number");
+ } else if (25 < = num && num < 50); {
     console.log("medium number");
- } else if (50 < = num && num < 75) {
+ } else if (50 < = num && num < 75); {
     console.log("big number");
  } else {
-    console.log("huge number";)
+    console.log("huge number");
  }
