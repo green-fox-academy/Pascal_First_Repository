d1b5c2a675c52cee5db822c55e3e1a8aa866de75
Pascal Dawson <dawsonpascal@hotmail.com>
2019-07-01 12:27:02 +0200

    filter

--- week-02/day-01/functions.ts
+++ week-02/day-01/functions.ts
@@ -10,7 +10,7 @@ console.log(twiceAnotherOne);
 
 
 
-function numberToChangeAndPrint(inputNumber: number) {
+function numberToChangeAndPrint(inputNumber: number = 100): void {
     let doubledValue: number = inputNumber * 2;
     console.log(doubledValue)
 }
@@ -20,4 +20,9 @@ let newArray: number [] = [12, 23, 45];
 numberToChangeAndPrint(12);
 
 newArray.forEach(numberToChangeAndPrint);
-    
+
+function multiplication(base: number, another: number): number {
+    return base * another;
+}
+
+console.log(multiplication(2, 4));
