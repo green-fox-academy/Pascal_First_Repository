117ddca251a92b16de6e410543fc03e0a046795b
Pascal Dawson <dawsonpascal@hotmail.com>
2019-07-02 17:22:29 +0200

    last one before checkout

---
+++ week-02/day-02/shopping-list.ts
@@ -0,0 +1,17 @@
+'use strict'
+
+let shoppingList: string[] = ['eggs', 'milk', 'apples', 'bread', 'chicken'];
+
+if (shoppingList.indexOf('milk') > 0) {
+    console.log('Yes we do.')
+} else {
+    console.log('No, we don\'t.')
+};
+
+if (shoppingList.indexOf('banana') > 0) {
+    console.log('Yes we do.')
+} else {
+    console.log('No, we don\'t.')
+};
+
+
