0873d959514df2d9ec997f828b2e28f295a87f12
Pascal Dawson <dawsonpascal@hotmail.com>
2019-07-09 17:24:12 +0200

    fuckloops

--- week-03/day-02/00-print-each-line.ts
+++ week-03/day-02/00-print-each-line.ts
@@ -5,8 +5,11 @@
 // If the program is unable to read the file (for example it does not exist),
 // then it should print the following error message: "Unable to read file: my-file.txt"
 
+declare function require(path: string): any;
+
 const fs = require('fs');
-let filename = 'my-file.txt'
+let filename: string = 'my-file.txt'
+let path = 'workshop/file-manipulations/assets/log.txt'
 
 try {
     let fileContent = fs.readFileSync(filename, 'utf-8');
--- week-03/day-02/05-logs.ts
+++ week-03/day-02/05-logs.ts
@@ -3,7 +3,14 @@ export = {};
 
 // Read all data from 'log.txt'.
 // Each line represents a log message from a web server
-// Write a function that returns an array with the unique IP adresses.
 // Write a function that returns the GET / POST request ratio.
+
+declare function require(path: string): any;
+const fs = require('fs');
+
+let tempString: string = fs.readFileSync('log.txt');
+let logArray: string[] = tempString.split('\n');
+
+// Write a function that returns an array with the unique IP adresses.
 
 
