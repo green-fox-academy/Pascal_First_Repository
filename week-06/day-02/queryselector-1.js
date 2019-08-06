c7fb7c799b69b131830f6abe17ab961b1073dd49
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-06 13:35:55 +0200

    first task done, still don't get DOM

---
+++ week-06/day-02/queryselector-1.js
@@ -0,0 +1,4 @@
+const king = document.getElementById('b325');
+const businessLamp = document.getElementsByClassName('big');
+const conceitedKing = document.querySelectorAll('.container > .asteroid');
+const noBusiness = document.querySelectorAll('div');
