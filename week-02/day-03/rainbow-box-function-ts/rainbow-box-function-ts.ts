d7e7d34ff88b9487b284054cae27c8f40703ea4c
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-06 15:04:43 +0200

    managed to solve a problem with canvases

--- week-02/day-03/rainbow-box-function-ts/rainbow-box-function-ts.ts
+++ week-02/day-03/rainbow-box-function-ts/rainbow-box-function-ts.ts
@@ -1,40 +1,8 @@
-"use strict";
+'use strict';
 
-const canvas = document.querySelector(".main-canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
 
-// DO NOT TOUCH THE CODE ABOVE THIS LINE
-
-// Create a square drawing function that takes 2 parameters:
-// The square size, and the fill color,
-// and draws a square of that size and color to the center of the canvas.
-// Create a loop that fills the canvas with a rainbow of colored squares.
-
-/*
-colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-
-*/
-
-/*
-function getRandomColor() {
-var letters = '0123456789ABCDEF';
-var color = '#';
-for (var i = 0; i < 6; i++) {
-color += letters[Math.floor(Math.random() * 16)];
-}
-return color;
-}
-
-*/
 const canvasWidth = 600;
 const canvasHeigth = 400;
 
@@ -42,12 +10,12 @@ function calcColor(min, max, val) {
   let minHue = 240,
     maxHue = 0;
   let curPercent = (val - min) / (max - min);
-  let color =
-    "hsl(" + (curPercent * (maxHue - minHue) + minHue) + ", 100%, 50%)";
-  return color;
+  let colorS =
+    'hsl(' + (curPercent * (maxHue - minHue) + minHue) + ', 100%, 50%)';
+  return colorS;
 }
 
-for (let i = 400; i > 0; i -= 1) {
+for (let i = 400; i > 0; i -= 10) {
   ctx.fillStyle = calcColor(240, 0, i);
   ctx.fillRect(canvasWidth / 2 - i / 2, canvasHeigth / 2 - i / 2, i, i);
 }
