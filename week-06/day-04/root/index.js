eea81ec5f6f23991d0f6fcd4e92e1f99f9dfa1a8
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-08 17:05:07 +0200

    not there yet

---
+++ week-06/day-04/root/index.js
@@ -0,0 +1,9 @@
+function navLeft() {
+  document.body.getElementsByClassName('navimg').setAttribute(src, '3.jpg');
+}
+
+function navRight() {
+  document.body.getElementsByClassName('navimg').setAttribute(src, '4.jpg');
+}
+
+function changeNavImg() {}
