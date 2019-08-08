7e14d7ff39fa26d93fb389593ec001ca0dffd879
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-08 17:42:40 +0200

    end of the day

--- week-06/day-04/root/index.js
+++ week-06/day-04/root/index.js
@@ -1,9 +1,33 @@
+let x = 2;
+let imgToUse = `${x}.jpg`;
+
 function navLeft() {
-  document.body.getElementsByClassName('navimg').setAttribute(src, '3.jpg');
+  if (document.body
+    .querySelectorAll('.navimg')[0].src === "1.jpg") {
+        x = 6
+    } else if(){
+    document.body
+      .querySelectorAll('.navimg')[0]
+      .setAttribute('src', `${x--}.jpg`);
+  } else {
+    x = 6;
+    document.body
+      .querySelectorAll('.navimg')[0]
+      .setAttribute('src', `${x--}.jpg`);
+  }
 }
 
 function navRight() {
-  document.body.getElementsByClassName('navimg').setAttribute(src, '4.jpg');
+  if (x < 6) {
+    document.body
+      .querySelectorAll('.navimg')[0]
+      .setAttribute('src', `${x++}.jpg`);
+  } else {
+    x = 1;
+    document.body
+      .querySelectorAll('.navimg')[0]
+      .setAttribute('src', `${x++}.jpg`);
+  }
 }
 
 function changeNavImg() {}
