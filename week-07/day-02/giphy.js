f470af3b713627e7f410b8153f43f23e7d87b012
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-24 19:37:26 +0200

    giphy almost done

---
+++ week-07/day-02/giphy.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function display(x) {
+  let page = document.querySelector('div');
+  if (page.length !== 0) {
+    while (page.firstChild) {
+      page.removeChild(page.firstChild);
+    }
+  }
+  for (let i = 0; i < x.length; i++) {
+    let element = document.createElement('img');
+    element.src = x[i].images.original.url;
+    element.style.height = '100px';
+    element.style.width = '100px';
+    page.appendChild(element);
+  }
+}
+const xhr = new XMLHttpRequest();
+
+document.querySelector('.button').addEventListener('click', e => {
+  e.preventDefault();
+  xhr.onload = function() {
+    let myData = JSON.parse(xhr.responseText);
+    display(myData.data);
+    console.log(myData);
+  };
+  let search = document.body.querySelector('input').value;
+  xhr.open(
+    'GET',
+    `https://api.giphy.com/v1/gifs/search?api_key=IcQpOHyvbeWUkfW5DbFMSriDoSTSNYef&q=${search}&limit=16&offset=0&rating=G&lang=en`
+  );
+  xhr.send();
+});
