a9c1f5b883caeff9188b954d910ab36be4ffce12
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-28 20:38:47 +0200

    star wars almost done, but it's beer time. remember to add an HTTP request into second eventlistener

--- week-07/day-02/starwars.js
+++ week-07/day-02/starwars.js
@@ -11,23 +11,39 @@ function deleting(list) {
 function display(x) {
   let page = document.querySelector('.characters');
   deleting(page);
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < x.length; i++) {
     let element = document.createElement('li');
-    element.textContent = 'temp';
+    element.textContent = x[i].name;
+    element.className = i;
     page.appendChild(element);
   }
 }
 
 const xhr = new XMLHttpRequest();
+let tempArray = [];
 
 document.querySelector('.button').addEventListener('click', e => {
   e.preventDefault();
   xhr.onload = function() {
     let myData = JSON.parse(xhr.responseText);
-    display(myData);
-    console.log(myData);
+    display(myData.results);
+    console.log(myData.results);
+    return (tempArray = myData.results);
   };
   let search = document.body.querySelector('input').value;
   xhr.open('GET', `https://swapi.co/api/people/?search=${search}`);
   xhr.send();
+});
+
+document.querySelector('.characters').addEventListener('click', e => {
+  let page = document.querySelector('.movies');
+  let index = e.target.className;
+  deleting(page);
+  for (let i = 0; i < x.length; i++) {
+    let element = document.createElement('li');
+    element.textContent = x[i].name;
+    element.className = i;
+    page.appendChild(element);
+  }
+  e.target;
 });
