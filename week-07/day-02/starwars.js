1d3efffde79ead4f3f8db017343b03da1c80732c
Pascal Dawson <dawsonpascal@hotmail.com>
2019-08-29 19:13:57 +0200

    star wars api done, altough the code is spaghetti

--- week-07/day-02/starwars.js
+++ week-07/day-02/starwars.js
@@ -10,7 +10,9 @@ function deleting(list) {
 
 function display(x) {
   let page = document.querySelector('.characters');
+  let pageSecond = document.querySelector('.movies');
   deleting(page);
+  deleting(pageSecond);
   for (let i = 0; i < x.length; i++) {
     let element = document.createElement('li');
     element.textContent = x[i].name;
@@ -19,15 +21,14 @@ function display(x) {
   }
 }
 
-const xhr = new XMLHttpRequest();
 let tempArray = [];
 
 document.querySelector('.button').addEventListener('click', e => {
+  const xhr = new XMLHttpRequest();
   e.preventDefault();
   xhr.onload = function() {
     let myData = JSON.parse(xhr.responseText);
     display(myData.results);
-    console.log(myData.results);
     return (tempArray = myData.results);
   };
   let search = document.body.querySelector('input').value;
@@ -38,12 +39,21 @@ document.querySelector('.button').addEventListener('click', e => {
 document.querySelector('.characters').addEventListener('click', e => {
   let page = document.querySelector('.movies');
   let index = e.target.className;
+  let arrayToUse = tempArray[index].films;
   deleting(page);
-  for (let i = 0; i < x.length; i++) {
-    let element = document.createElement('li');
-    element.textContent = x[i].name;
-    element.className = i;
-    page.appendChild(element);
+  for (let i = 0; i < arrayToUse.length; i++) {
+    const xhr = new XMLHttpRequest();
+    e.preventDefault();
+    xhr.onload = function() {
+      let myData = JSON.parse(xhr.responseText);
+      let element = document.createElement('li');
+      element.textContent = `${myData.title} (${myData.release_date})`;
+      page.appendChild(element);
+      console.log(myData);
+    };
+    let search = document.body.querySelector('input').value;
+    xhr.open('GET', `${arrayToUse[i]}`);
+    xhr.send();
   }
   e.target;
 });
